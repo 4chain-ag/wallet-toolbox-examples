357f850fcdea9d39b94e80f600651cfd4800292e
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-03-12 15:27:40 +0100

    feat: add backup remote example

---
+++ src/backupRemote.ts
@@ -0,0 +1,43 @@
+import {sdk, Setup, SetupEnv, SetupWallet, StorageClient} from '@bsv/wallet-toolbox'
+import { runArgv2Function } from './runArgv2Function'
+
+/**
+ * @publicbody
+ */
+export async function backup(): Promise<void> {
+  const env = Setup.getEnv('test')
+  await backupWalletClient(env, env.identityKey)
+}
+
+/**
+ * @publicbody
+ */
+export async function backupWalletClient(
+  env: SetupEnv,
+  identityKey: string
+): Promise<void> {
+  const setup = await Setup.createWalletClient({
+    env,
+    rootKeyHex: env.devKeys[identityKey]
+  })
+  await backupToSQLite(setup)
+  await setup.wallet.destroy()
+}
+
+/**
+ * @publicbody
+ */
+export async function backupToSQLite(
+  setup: SetupWallet,
+  filePath?: string,
+  databaseName?: string
+): Promise<void> {
+
+  const backup = new StorageClient(setup.wallet, 'http://localhost:8100')
+
+  await setup.storage.addWalletStorageProvider(backup)
+  await setup.storage.setActive("02a36812c25aa167c4f23eaeecc8b91c89d5e06b278db11e53c11f57f6f1fd4ca5")
+  await setup.storage.updateBackups()
+}
+
+runArgv2Function(module.exports)
--- src/faucetAddress.ts
+++ src/faucetAddress.ts
@@ -1,27 +1,29 @@
 import {brc29ProtocolID, Setup} from "@bsv/wallet-toolbox";
 
-async function faucetAddress() {
-  const env = Setup.getEnv('test')
+type Chain = 'main' | 'test'
+
+async function faucetAddress(network: Chain) {
+  const env = Setup.getEnv(network)
   const setup = await Setup.createWalletClient({ env })
 
-  const derivationPrefix = 'SfKxPIJNgdI=';
-  const derivationSuffix = 'NaGLC6fMH50=';
+  const derivationPrefix = 'SfKxPIJNgdI='
+  const derivationSuffix = 'NaGLC6fMH50='
   const keyId = `${derivationPrefix} ${derivationSuffix}`
 
   const address = setup.keyDeriver
     .derivePrivateKey(brc29ProtocolID, keyId, 'anyone')
-    .toAddress('testnet')
+    .toAddress(`${network}net`)
 
-  console.log("====================================")
-  console.log("")
-  console.info("Below is the address that you should top up from faucet:")
-  console.log("")
+  console.log('====================================')
+  console.log('')
+  console.info('Below is the address that you should top up from faucet:')
+  console.log('')
   console.info(address.toString())
-  console.log("")
-  console.info("You can use one of those testnet faucets:")
-  console.info("https://scrypt.io/faucet")
-  console.info("https://witnessonchain.com/faucet/tbsv")
+  console.log('')
+  console.info('You can use one of those testnet faucets:')
+  console.info('https://scrypt.io/faucet')
+  console.info('https://witnessonchain.com/faucet/tbsv')
 }
 
 
-faucetAddress().catch(console.error)
+faucetAddress('test').catch(console.error)
